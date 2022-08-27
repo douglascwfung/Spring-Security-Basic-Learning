af8d8f2df58d5c8e8c5a597572145a1960676e05
Douglas Fung <icestone.net@gmail.com>
2022-08-27 14:44:57 +0800

    add spring security in pom.xml and add login and password in application.properties

--- src/main/java/net/icestone/springsecuritybasic/controller/WelcomeController.java
+++ src/main/java/net/icestone/springsecuritybasic/controller/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 	
-	@GetMapping("/Welcome")
+	@GetMapping("/welcome")
 	public String sayWelcome() {
 		return "Welcome from Spring Application with Security";
 	}
