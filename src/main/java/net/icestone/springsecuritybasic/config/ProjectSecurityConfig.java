b39a7b1db72d9763f555959221538036f5f5953d
Douglas Fung <icestone.net@gmail.com>
2022-08-28 08:09:47 +0800

    Section 3.17 using inMemoryAuthentication

---
+++ src/main/java/net/icestone/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -0,0 +1,73 @@
+package net.icestone.springsecuritybasic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig  {
+
+
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests( (auth)->auth
+				.antMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+				.antMatchers("/notices","/contact").permitAll()
+				.anyRequest().authenticated()
+		)
+		.httpBasic(Customizer.withDefaults());
+		return http.build();
+	}
+
+	
+ /**
+ * Approach 1 where we use withDefaultPasswordEncoder() method
+ * while creating the user details
+ */
+//	@Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//
+//    	UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("12345")
+//                .authorities("admin")
+//                .build();
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("12345")
+//                .authorities("read")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
+
+	
+    /*
+    * Approach 2 where we don't define password encoder
+    * while creating the user details. Instead a separate
+    * PasswordEncoder bean will be created.
+    */
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails admin = User.withUsername("admin").password("12345").authorities("admin").build();
+        UserDetails user = User.withUsername("user").password("12345").authorities("read").build();
+        userDetailsService.createUser(admin);
+        userDetailsService.createUser(user);
+        return userDetailsService;
+    }
+	
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    
+}
